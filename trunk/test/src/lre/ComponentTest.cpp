e69e8257eb8b69b440f9a1872fce3120e7ae9f67
vtxtech@gmail.com <vtxtech@gmail.com@d56bdc5b-ba41-d1ce-baa2-a4ab1eb79a3e>
2013-01-10 19:56:35 +0000

    Updated file headers

--- trunk/test/src/lre/ComponentTest.cpp
+++ trunk/test/src/lre/ComponentTest.cpp
@@ -3,7 +3,7 @@
  * Copyright (C) 2012-2013 Johannes Scholz. All rights reserved.
  *
  * This file is licensed under the GNU Lesser General Public License 3 (LGPLv3),
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * but distributed WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *
  */
