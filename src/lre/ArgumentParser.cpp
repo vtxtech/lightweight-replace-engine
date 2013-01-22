f1eb302cd70cc5f9f0b772ed036e0a89f9f5275a
vtxtech <vtxtech@gmail.com>
2013-01-22 08:26:10 +0000

    Formatting of lre::ArgumentParser::reportOptions()

--- src/lre/ArgumentParser.cpp
+++ src/lre/ArgumentParser.cpp
@@ -99,15 +99,78 @@ namespace lre {
 		options_.insert(StringStringPair(argument, description));
 	}
 
+	enum FillSide {
+		Left,
+		Right
+	};
+
+	std::string fill(const std::string& input, int targetWidth, FillSide side)
+	{
+		std::string output = input;
+		while (output.size() < targetWidth) {
+			if (side == Left) {
+				output = " " + output;
+			} else {
+				output += " ";
+			}
+		}
+		return output;
+	}
+
 	//=======================================================================================
 	void ArgumentParser::reportOptions()
 	{
 		if (appName_ != "") { std::cout<<appName_<<std::endl; }
-		if (appUsage_ != "") { std::cout<<"Usage: "<<lre::FileUtils::extractFilename(arguments_.at(0))<<" "<<appUsage_<<std::endl; }
+		if (appUsage_ != "") { std::cout<<"Usage: "<<lre::FileUtils::extractFilename(arguments_.at(0))<<" "<<appUsage_<<std::endl<<std::endl; }
+
+		int terminalWidth = 80;
+		int leftColumnWidth = 5;
+		int rightColumnWidth = terminalWidth - leftColumnWidth;
 
 		for (StringStringMap::iterator itr = options_.begin(); itr != options_.end(); ++itr) {
-			std::cout<<itr->first<<":"<<std::endl;
-			std::cout<<itr->second<<std::endl<<std::endl;
+			// print option, e.g. --input <foo>
+			std::string param = fill(itr->first + ": ", leftColumnWidth, Right);
+			std::cout<<param;
+			// fill the left column of the next line after the option
+			// with spaces, so the description does not have to take
+			// care of whether it's in the same line with the option
+			// or in the next line
+			if (param.size() > leftColumnWidth) {
+				std::cout<<std::endl<<std::endl<<fill("", leftColumnWidth, Left);
+			}
+			// now print the description
+			std::string descr = itr->second;
+			if (descr.size() > rightColumnWidth) {
+				// find out what fits into the right column
+				std::string out = descr.substr(0, rightColumnWidth);
+				// well, now find the last space so we do not break within a word
+				std::string::size_type p = out.find_last_of(" ");
+				std::cout<<fill(out.substr(0, p), terminalWidth-leftColumnWidth, Right);
+				// skip the space so we do not start our next line with it
+				p++;
+				// descr now holds only the rest of the text that isn't
+				// printed, yet.
+				descr = descr.substr(p, descr.size() - p);
+				// look until we have only the final line left
+				while (descr.size() > rightColumnWidth) {
+					// again, find out what fits into the right column
+					out = descr.substr(0, rightColumnWidth);
+					// find the last space ...
+					p = out.find_last_of(" ");
+					out = out.substr(0, p);
+					// ... and print it!
+					std::cout<<fill(fill(out, leftColumnWidth+out.size(), Left), terminalWidth, Right);
+					// skip the space
+					p++;
+					descr = descr.substr(p, descr.size() - p);
+				}
+				// ok, finally print out the last line
+				int lastLineSize = leftColumnWidth+descr.size();
+				std::cout<<fill(descr.substr(0, rightColumnWidth), lastLineSize, Left);
+				if (lastLineSize != terminalWidth) { std::cout<<std::endl<<std::endl; }
+			} else {
+				std::cout<<descr<<std::endl<<std::endl;
+			}
 		}
 	}
 
